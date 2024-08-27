9065a1148368afbe0e4b239022f51a7b7a22d74b
Liz Lee <aihsliz31@gmail.com>
2024-08-27 12:46:53 +0900

    오류가 있지만 일단 api 파일 내용 업로드

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
+import os
 from flask import Flask
 from flask import render_template
+from models import db
 from api_v1 import api as api_v1
 
 app = Flask(__name__)
-app.register_blueprint(api_v1, url_prefix='api/v1')
+app.register_blueprint(api_v1, url_prefix='/api/v1')
 
 @app.route('/register')
 def register():
@@ -14,6 +16,19 @@ def register():
 def hello():
     return 'Hello World'
 
+basedir = os.path.abspath(os.path.dirname(__file__))
+dbfile = os.path.join(basedir, 'db.sqlite')
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + dbfile
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True  # Commit on teardown
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # Suppress warning messages
+app.config['SECRET_KEY'] = 'adfhalskdjfl;asdjriern' #for CSRF
+
+db.init_app(app)
+
+# Ensure the following block runs within an app context
+with app.app_context():
+    db.create_all()  # Create the database tables
 
 
 if __name__ == "__main__" :
